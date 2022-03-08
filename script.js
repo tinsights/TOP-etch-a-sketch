5278c5c8cf258b9440a65f614aab6066b0a75063
tinsights <aj.tinaes@gmail.com>
2022-03-08 12:07:01 +0800

    hover to change color

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ for (let i = 0; i < 16; i +=1) {
   for (let j = 0; j < 16; j += 1) {
     const square = document.createElement('div');
     square.classList.toggle("square");
+    square.addEventListener("mouseover", () => {
+      square.classList.toggle("coloured");
+    })
     row.appendChild(square);
   }
   GRID_CONTAINER.appendChild(row);
