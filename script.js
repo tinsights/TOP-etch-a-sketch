5544b51691aa12573fa2766ec46648a625dd88b5
tinsights <aj.tinaes@gmail.com>
2022-03-08 11:57:42 +0800

    basic 16x16 grid

---
+++ script.js
@@ -0,0 +1,20 @@
+const FLEX_CONTAINER = document.createElement('div');
+const GRID_CONTAINER = document.createElement('div');
+
+FLEX_CONTAINER.setAttribute("id", "flex-container");
+GRID_CONTAINER.setAttribute("id", "grid-container")
+
+for (let i = 0; i < 16; i +=1) {
+  const row = document.createElement("div");
+  row.classList.toggle("row");
+  for (let j = 0; j < 16; j += 1) {
+    const square = document.createElement('div');
+    square.classList.toggle("square");
+    row.appendChild(square);
+  }
+  GRID_CONTAINER.appendChild(row);
+}
+
+
+FLEX_CONTAINER.appendChild(GRID_CONTAINER);
+document.body.appendChild(FLEX_CONTAINER);
